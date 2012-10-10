a92521ce029d4d760509ace97587ea14ba20fcc0
sungeoliu <liuwanwei@sungeo.cn>
2012-10-10 11:30:13 +0800

    creating project

---
+++ HappyBaby/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HappyBaby
+//
+//  Created by Liu Wanwei on 12-10-10.
+//  Copyright (c) 2012年 Liu Wanwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HappyBaby/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HappyBaby
+//
+//  Created by Liu Wanwei on 12-10-10.
+//  Copyright (c) 2012年 Liu Wanwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
