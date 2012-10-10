b8019ef28e74f9ae19631decf60cb0b6bf0b2fb9
sungeoliu <liuwanwei@sungeo.cn>
2012-10-10 12:15:54 +0800

    recreate project supporting coredata

---
+++ HappyBabyTests/HappyBabyTests.h
@@ -0,0 +1,13 @@
+//
+//  HappyBabyTests.h
+//  HappyBabyTests
+//
+//  Created by Liu Wanwei on 12-10-10.
+//  Copyright (c) 2012年 Liu&Mao. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HappyBabyTests : SenTestCase
+
+@end
---
+++ HappyBabyTests/HappyBabyTests.m
@@ -0,0 +1,32 @@
+//
+//  HappyBabyTests.m
+//  HappyBabyTests
+//
+//  Created by Liu Wanwei on 12-10-10.
+//  Copyright (c) 2012年 Liu&Mao. All rights reserved.
+//
+
+#import "HappyBabyTests.h"
+
+@implementation HappyBabyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HappyBabyTests");
+}
+
+@end
