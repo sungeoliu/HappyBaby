a8b2c925c1a94df4eb3a7aebe7d136b328dab2c6
mao yu <maoyu417@163.com>
2012-10-11 08:10:36 +0800

    添加项目新资源

---
+++ HappyBaby/Classes/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  HappyBaby
+//
+//  Created by lixiaoyu on 12-10-10.
+//  Copyright (c) 2012年 Liu&Mao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
---
+++ HappyBaby/Classes/HomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.m
+//  HappyBaby
+//
+//  Created by lixiaoyu on 12-10-10.
+//  Copyright (c) 2012年 Liu&Mao. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
