566d1616bd5494eed7956661e146daca1917594a
mao yu <maoyu417@163.com>
2012-10-17 18:27:08 +0800

    项目新文件

---
+++ HappyBaby/UIExtendNavigationController.m
@@ -0,0 +1,53 @@
+//
+//  UIExtendNavigationController.m
+//  HappyBaby
+//
+//  Created by lixiaoyu on 12-10-17.
+//  Copyright (c) 2012年 Liu&Mao. All rights reserved.
+//
+
+#import "UIExtendNavigationController.h"
+
+@interface UIExtendNavigationController ()
+
+@end
+
+@implementation UIExtendNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(BOOL)shouldAutorotate
+{
+    return [[self.viewControllers lastObject] shouldAutorotate];
+}
+
+-(NSUInteger)supportedInterfaceOrientations
+{
+    return [[self.viewControllers lastObject] supportedInterfaceOrientations];
+}
+
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
+{
+    return [[self.viewControllers lastObject] preferredInterfaceOrientationForPresentation];
+}
+
+@end
