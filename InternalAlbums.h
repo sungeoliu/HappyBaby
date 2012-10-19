9adb7677d352a037c9c0bbeaa22e1290f3ad4562
sungeoliu <liuwanwei@sungeo.cn>
2012-10-19 15:36:17 +0800

    1,添加专辑描述类Album和自带专辑管理类InternalAlbums。 2,增加创建“家庭”专辑的代码。 3,增加对多语言的支持，表现在专辑信息描述文件Album.strings。

---
+++ InternalAlbums.h
@@ -0,0 +1,15 @@
+//
+//  InternalAlbum.h
+//  HappyBaby
+//
+//  Created by Liu Wanwei on 12-10-19.
+//  Copyright (c) 2012年 Liu&Mao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InternalAlbums : NSObject
+
+@property (nonatomic, strong, readonly) NSArray * albums;
+
+@end
