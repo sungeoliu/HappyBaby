cb36956aec445d70c2a2a67e5b34be7f042411a9
mao yu <maoyu417@163.com>
2012-10-27 08:21:41 +0800

    支持照片修改、录音功能

--- QuestionModeGameViewController.h
+++ QuestionModeGameViewController.h
@@ -9,14 +9,24 @@
 #import "BaseGameViewController.h"
 #import "GameEngine.h"
 
-@interface QuestionModeGameViewController : BaseGameViewController<GameEngineDelegate,UIAlertViewDelegate>
+@interface QuestionModeGameViewController : BaseGameViewController<GameEngineDelegate,UIAlertViewDelegate,UIActionSheetDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 
 @property(weak, nonatomic) IBOutlet UILabel * labelInfo;
 @property(weak, nonatomic) IBOutlet UILabel * labelCountdown;
+@property(weak, nonatomic) IBOutlet UIButton * buttonAudioPlay;
+@property(weak, nonatomic) IBOutlet UIButton * buttonAudioRecord;
+@property(weak, nonatomic) IBOutlet UIButton * buttonEdit;
+@property(weak, nonatomic) IBOutlet UIButton * buttonPhoto;
+@property(weak, nonatomic) IBOutlet UIImageView * imageViewPhoto;
+@property(weak, nonatomic) IBOutlet UIView * viewBackground;
 
 @property(nonatomic) GameMode gameMode;
 
+- (IBAction)playAudioPlay:(UIButton *)sender;
 - (IBAction)checkAnswer:(UIButton *)sender;
 - (IBAction)newCard:(UIButton *)sender;
+- (IBAction)editCard:(UIButton *)sender;
+- (IBAction)modifyPhoto:(UIButton *)sender;
+- (IBAction)modifyAudio:(UIButton *)sender;
 
 @end
--- QuestionModeGameViewController.m
+++ QuestionModeGameViewController.m
@@ -8,6 +8,7 @@
 
 #import "QuestionModeGameViewController.h"
 #import "SoundManager.h"
+#import "CardAudioRecordViewController.h"
 
 @interface QuestionModeGameViewController () {
     GameEngine * _gameEngine;
@@ -15,12 +16,21 @@
     NSInteger  _curSelectCardIndex;
     NSNumber * _curSelectCardId;
     NSArray * _options;
+    Question * _question;
+    BOOL _isEdit;
 }
 
 @end
 
 @implementation QuestionModeGameViewController
 @synthesize labelCountdown = _labelCountdown;
+@synthesize labelInfo = _labelInfo;
+@synthesize buttonAudioPlay = _buttonAudioPlay;
+@synthesize buttonAudioRecord = _buttonAudioRecord;
+@synthesize buttonEdit = _buttonEdit;
+@synthesize buttonPhoto = _buttonPhoto;
+@synthesize imageViewPhoto = _imageViewPhoto;
+@synthesize viewBackground = _viewBackground;
 
 #pragma 私有函数
 - (void)showAlert {
@@ -30,6 +40,7 @@
 }
 // 检查卡片图片、录音是否进行设置
 - (BOOL)checkCards {
+    return YES;
     if (nil != self.cards) {
         NSInteger count = self.cards.count;
         Card * card;
@@ -62,10 +73,13 @@
             card = [self cardWithId:cardId];
             
             button = (UIButton *)[self.view viewWithTag:index + 1];
-            url = [[NSURL alloc] initWithString:card.image];
-            data = [NSData dataWithContentsOfURL:url];
-            image = [UIImage imageWithData:data];
-            [button setBackgroundImage:image forState:UIControlStateNormal];
+            [self showShadowWithButton:button];
+            if (nil != card.image) {
+                url = [[NSURL alloc] initWithString:card.image];
+                data = [NSData dataWithContentsOfURL:url];
+                image = [UIImage imageWithData:data];
+                [button setBackgroundImage:image forState:UIControlStateNormal];
+            }
             
             label = (UILabel *)[self.view viewWithTag:-1 - index];
             label.text = card.name;
@@ -93,6 +107,7 @@
 
 - (void)registerHandleMessage {
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleMessage:) name:kSoundPlaySuccessMessage object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleRecordSucessMessage:) name:kSoundRecordSuccessMessage object:nil];
 }
 
 - (void)handleMessage:(NSNotification *)note {
@@ -106,6 +121,47 @@
         default:
             break;
     }
+}
+
+- (void)handleRecordSucessMessage:(NSNotification *)note {
+    [self initCardsWithAlbum:self.albumType];
+     Card * card =  [self cardWithId:[_question.options objectAtIndex:0]];
+    _question.voice = [[NSURL alloc] initWithString:card.pronunciation];
+}
+
+- (void)showActionSheet {
+    UIActionSheet * menu = [[UIActionSheet alloc]
+                            initWithTitle:nil delegate:self
+                            cancelButtonTitle:@"取消"
+                            destructiveButtonTitle:nil
+                            otherButtonTitles:@"相册", @"相机", nil];
+    
+    [menu showInView:self.view];
+}
+
+- (void)showPhotoWithType:(UIImagePickerControllerSourceType) type {
+    UIImagePickerController * imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.delegate = self;
+    if ([UIImagePickerController isSourceTypeAvailable:type]) {
+        imagePickerController.sourceType = type;
+        imagePickerController.allowsEditing = YES;
+        [self presentViewController:imagePickerController animated:YES completion:nil];
+    }
+}
+
+- (void)showCardAudioRecordView {
+    CardAudioRecordViewController * viewController = [[CardAudioRecordViewController alloc] initWithNibName:@"CardAudioRecordViewController" bundle:nil];
+    
+    Card * card =  [self cardWithId:[_question.options objectAtIndex:0]];
+    viewController.card = card;
+    
+    //CGRect fream = CGRectMake(15.0, 100.0, 284.0, 230.0);
+    //viewController.view.frame = fream;
+    //viewController.view.alpha = 1;
+    
+    [self.view addSubview:viewController.view];
+    [self.view addSubview:_viewBackground];
+    [self addChildViewController:viewController];
 }
 
 #pragma 事件函数
@@ -125,6 +181,7 @@
     [super viewDidLoad];
     _gameEngine.answerQuestionTimerInterval = 10;
     _gameEngine.gameMode = self.gameMode;
+    _isEdit = NO;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -166,12 +223,42 @@
     }
 }
 
+- (IBAction)playAudioPlay:(UIButton *)sender {
+    [_soundManager playSound:_question.voice];
+}
+
 - (IBAction)newCard:(UIButton *)sender {
     [_gameEngine newQuestion];
 }
 
+- (IBAction)editCard:(UIButton *)sender {
+    if (NO == _isEdit) {
+        [_buttonEdit setTitle:@"完成" forState:UIControlStateNormal];
+        [_buttonPhoto setHidden:NO];
+        [_imageViewPhoto setHidden:NO];
+        [_buttonAudioRecord setHidden:NO];
+        [_soundManager stopBackgroundSound];
+    }else {
+        [_buttonEdit setTitle:@"编辑" forState:UIControlStateNormal];
+        [_buttonPhoto setHidden:YES];
+        [_imageViewPhoto setHidden:YES];
+        [_buttonAudioRecord setHidden:YES];
+        [_soundManager playBackgroundSound];
+    }
+    
+    _isEdit = !_isEdit;
+}
+
+- (IBAction)modifyPhoto:(UIButton *)sender {
+    [self showActionSheet];
+}
+
+- (IBAction)modifyAudio:(UIButton *)sender {
+    [self showCardAudioRecordView];
+}
+
 -(NSUInteger)supportedInterfaceOrientations {
-    return UIInterfaceOrientationMaskLandscape;
+    return UIInterfaceOrientationMaskPortrait;
 }
 
 - (BOOL)shouldAutorotate {
@@ -183,7 +270,11 @@
     if (nil != question) {
         [self.labelInfo setText:question.prompt];
     }
-    [_soundManager playSound:question.voice];
+    _question = question;
+    if (nil != question.voice) {
+        [_soundManager playSound:question.voice];
+    }
+  
     [self initViewWithOptions:question.options];
     self.answerState = AnswerStateWait;
 }
@@ -219,5 +310,28 @@
         [self back:nil];
     }
 }
+
+#pragma mark - ActionSheet view delegate
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if(buttonIndex == 0) {
+        [self showPhotoWithType:UIImagePickerControllerSourceTypePhotoLibrary];
+    }else if(buttonIndex == 1){
+        [self showPhotoWithType:UIImagePickerControllerSourceTypeCamera];
+    }
+}
+
+#pragma mark - ImagePicker delegate
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage * image =  [info valueForKey:UIImagePickerControllerEditedImage];
+    UIButton  * button =  (UIButton *)[self.view viewWithTag:1];
+    [button setImage:image forState:UIControlStateNormal];
+    Card * card =  [self cardWithId:[_question.options objectAtIndex:0]];
+    [[CardManager defaultManager] modifyCard:card withImage:image];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
 
 @end
