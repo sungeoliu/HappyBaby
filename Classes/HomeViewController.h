a0c05c424dae62c26e3ae2861b902e6cd24ad85e
mao yu <maoyu417@163.com>
2012-10-31 18:06:17 +0800

    添加buttonSet属性

--- Classes/HomeViewController.h
+++ Classes/HomeViewController.h
@@ -12,13 +12,13 @@
 
 @property (weak, nonatomic) IBOutlet UIButton * buttonStudy;
 @property (weak, nonatomic) IBOutlet UIButton * buttonGame;
+@property (weak, nonatomic) IBOutlet UIButton * buttonSet;
 @property (weak, nonatomic) IBOutlet UILabel * labelTop;
 @property (weak, nonatomic) IBOutlet UILabel * labelBottom;
-@property (weak, nonatomic) UISegmentedControl * segmentedControl;
 
 - (IBAction)startGame:(UIButton *)sender;
 - (IBAction)setCard:(UIButton *)sender;
-- (IBAction)gameModeChanged:(UISegmentedControl *)sender;
+- (IBAction)cardManager:(UIButton *)sender;
 
 - (IBAction)touchDown:(UIButton *)sender;
 - (IBAction)touchCancel:(UIButton *)sender;
--- Classes/HomeViewController.m
+++ Classes/HomeViewController.m
@@ -8,10 +8,11 @@
 
 #import "HomeViewController.h"
 #import "QuestionModeGameViewController.h"
-#import "CardManagerViewController.h"
 #import "GameEngine.h"
 #import "SoundManager.h"
 #import "Feature.h"
+#import "StudyModeGameViewController.h"
+#import "CardManagerViewController.h"
 
 @interface HomeViewController () {
     GameMode _gameMode;
@@ -22,13 +23,17 @@
 @implementation HomeViewController
 @synthesize buttonStudy = _buttonStudy;
 @synthesize buttonGame = _buttonGame;
-@synthesize segmentedControl = _segmentedControl;
 @synthesize labelBottom = _labelBottom;
 @synthesize labelTop = _labelTop;
+@synthesize buttonSet = _buttonSet;
 
 #pragma 私有函数
 - (void)setBackgroundColorWithLabel:(UILabel *)label {
     [label setBackgroundColor:[[Feature defaultFeature] pinkColor]];
+}
+
+- (void)setBackgroundColorWithButton:(UIButton *)button {
+    [button setBackgroundColor:[[Feature defaultFeature] pinkColor]];
 }
 
 - (void)showShadowAndRadius:(UIButton *)button {
@@ -52,26 +57,45 @@
 }
 
 - (void)handleMessage:(NSNotification *)note {
+    [self showGameViewController];
+}
+
+- (void)showGameViewController {
     NSString * nib = [[NSString alloc] initWithFormat:@"QuestionMode%dGameViewController",_gameMode];
     QuestionModeGameViewController * viewController = [[QuestionModeGameViewController alloc] initWithNibName:nib bundle:nil];
     viewController.albumType = AlbumTypeFamily;
     viewController.gameMode = _gameMode;
     
-    /*[UIView beginAnimations:@"animationID" context:nil];//开始一个动画块，第一个参数为动画块标识
-    [UIView setAnimationDuration:1.5f];//设置动画的持续时间
-    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
-    //设置动画块中的动画属性变化的曲线，此方法必须在beginAnimations方法和commitAnimations，默认即为UIViewAnimationCurveEaseInOut效果。
-    //详细请参见UIViewAnimationCurve
-    [UIView setAnimationRepeatAutoreverses:NO];//设置是否自动反转当前的动画效果
-    [UIView setAnimationTransition:UIViewAnimationTransitionFlipFromLeft forView:self.view cache:YES];
-    //设置过渡的动画效果，此处第一个参数可使用上面5种动画效果。
-    [self.view exchangeSubviewAtIndex:1 withSubviewAtIndex:0];//页面翻转
-    [self.navigationController pushViewController:viewController animated:NO];
-    [UIView commitAnimations];//提交动画*/
-     [self.navigationController pushViewController:viewController animated:YES];
+    [self.navigationController pushViewController:viewController animated:YES];
+}
+
+// 检查卡片图片、录音是否进行设置
+- (BOOL)checkCards {
+    NSArray * cards = [[CardManager defaultManager] allCardsInAlbum:AlbumTypeFamily];
+    if (nil != cards) {
+        NSInteger count = cards.count;
+        Card * card;
+        for (NSInteger index = 0; index < count; index++) {
+            card = [cards objectAtIndex:index];
+            if (nil == card.image || nil == card.pronunciation) {
+                [self showAlert];
+                return NO;
+            }
+        }
+    }else {
+        return NO;
+    }
+    
+    return YES;
+}
+
+- (void)showAlert {
+    UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"提示" message:@"请设置所有卡片照片并录制声音" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+    [alert show];
     
 }
 
+#pragma 事件函数
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -86,16 +110,23 @@
     [super viewDidLoad];
     [self showShadowAndRadius:_buttonStudy];
     [self showShadowAndRadius:_buttonGame];
+    [self showShadowAndRadius:_buttonSet];
     [self setBackgroundColorWithLabel:_labelTop];
     [self setBackgroundColorWithLabel:_labelBottom];
+    [self setBackgroundColorWithButton:_buttonStudy];
+    [self setBackgroundColorWithButton:_buttonGame];
+    [self setBackgroundColorWithButton:_buttonSet];
+    [self.navigationController setNavigationBarHidden:YES];
+
+    if (NO == [self checkCards]) {
+        [self cardManager:nil];
+    }
     _gameMode = GameModeOneOption;
-    // Do any additional setup after loading the view from its nib.
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     [self registerHandleMessage];
-    [self.navigationController setNavigationBarHidden:YES];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
@@ -135,19 +166,30 @@
 }
 
 - (IBAction)startGame:(UIButton *)sender {
-    _gameMode = sender.tag + GameModeOneOption;
-    [self showShadowAndRadius:sender];
-    [[SoundManager defaultManager] playSystemSound:SystemSoundReady];
+    if (YES == [self checkCards]) {
+        _gameMode = sender.tag + GameModeOneOption;
+        [self showShadowAndRadius:sender];
+        [[SoundManager defaultManager] playSystemSound:SystemSoundReady];
+    }
 }
 
 - (IBAction)setCard:(UIButton *)sender {
+    if (YES == [self checkCards]) {
+        _gameMode = sender.tag + GameModeOneOption;
+        [self showShadowAndRadius:sender];
+        NSString * nib = [[NSString alloc] initWithFormat:@"QuestionMode%dGameViewController",_gameMode];
+        StudyModeGameViewController * viewController = [[StudyModeGameViewController alloc] initWithNibName:nib bundle:nil];
+        viewController.albumType = AlbumTypeFamily;
+        viewController.gameMode = _gameMode;
+    
+        [self.navigationController pushViewController:viewController animated:YES];
+    }
+}
+
+- (IBAction)cardManager:(UIButton *)sender {
+    [self showShadowAndRadius:sender];
     CardManagerViewController * viewController = [[CardManagerViewController alloc] initWithNibName:@"CardManagerViewController" bundle:nil];
     viewController.albumType = AlbumTypeFamily;
     [self.navigationController pushViewController:viewController animated:YES];
 }
-
-- (IBAction)gameModeChanged:(UISegmentedControl *)sender {
-    _gameMode = sender.selectedSegmentIndex + GameModeOneOption;
-}
-
 @end
